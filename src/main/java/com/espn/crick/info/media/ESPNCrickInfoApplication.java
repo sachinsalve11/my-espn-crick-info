a7dd0d31d3aa9d204367ed86879fa500a1de8feb
Sachin Salve <52715007+sachinsalve11@users.noreply.github.com>
2024-08-15 17:08:31 +0530

    Commit1 # My ESP Application.

---
+++ src/main/java/com/espn/crick/info/media/ESPNCrickInfoApplication.java
@@ -0,0 +1,14 @@
+package com.espn.crick.info.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
+public class ESPNCrickInfoApplication {
+
+    public  static void main(String[] args){
+        SpringApplication.run(ESPNCrickInfoApplication.class);
+    }
+}
